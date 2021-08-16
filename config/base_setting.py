0f427dfa608eb90b08a9fd448611e19a4001c5fa
fan <2642413273@qq.com>
2021-08-16 21:57:02 +0800

    Initial commit

---
+++ config/base_setting.py
@@ -0,0 +1,7 @@
+# 端口号
+SERVER_PORT = 8999
+# 关闭debug模式
+DEBUG = False
+# 不记录打印SQL语句
+SQLALCHEMY_ECHO = False
+
---
+++ config/local_setting.py
@@ -0,0 +1,10 @@
+# 开启debug模式
+DEBUG = True
+# 记录打印SQL语句用于调试
+SQLALCHEMY_ECHO = True
+# 链接数据库
+SQLALCHEMY_DATABASE_URI = 'mysql://root:a148148.@127.0.0.1/mysql'
+# 关闭数据库修改的跟踪
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+# 编码格式
+SQLALCHEMY_ENCODING = "utf-8"
